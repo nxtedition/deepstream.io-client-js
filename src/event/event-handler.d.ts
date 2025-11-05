dc99d647f919ea0b8b97a5e41ec0e9f4447132ad
Björn Norrliden <bjorn.norrliden@gmail.com>
2025-11-05 14:25:37 +0100

    fix(types): ds.event callback arg

--- src/event/event-handler.d.ts
+++ src/event/event-handler.d.ts
@@ -3,11 +3,11 @@ import { Observable } from 'rxjs'
 export default class EventHandler {
   connected: boolean
   stats: EventStats
-  subscribe: (name: string, callback: () => void) => void
-  unsubscribe: (name: string, callback: () => void) => void
-  on: (name: string, callback: () => void) => this
-  once: (name: string, callback: () => void) => this
-  off: (name: string, callback: () => void) => this
+  subscribe: (name: string, callback: (data: unknown) => void) => void
+  unsubscribe: (name: string, callback: (data: unknown) => void) => void
+  on: (name: string, callback: (data: unknown) => void) => this
+  once: (name: string, callback: (data: unknown) => void) => this
+  off: (name: string, callback: (data: unknown) => void) => this
   observe: <Data>(name: string) => Observable<Data>
   emit: <Data>(name: string, data?: Data) => void
   provide: (pattern: string, callback: (name: string) => void, options: unknown) => () => void
